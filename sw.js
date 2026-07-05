9549bac66ecb98ab31a0faa37a03d06bfaa790b0
KevSanNoUmi <t56mh56fdt@privaterelay.appleid.com>
2026-07-05 22:00:21 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const C='carnet-japon-v12';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest'])));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(n=>{if(e.request.method==='GET'&&n.ok){const cl=n.clone();caches.open(C).then(c=>c.put(e.request,cl));}return n;}).catch(()=>caches.match('./index.html'))));});
